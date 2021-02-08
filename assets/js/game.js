7b5dbd3df27f3359c2cf218812734386d5138658
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-02-08 13:35:00 -0700

    add variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,28 @@
-window.alert("This is an alert! JavaScript is running!");
+window.prompt("What is your robot's name?");
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+var PlayerName = window.prompt("What is your robot's name?");
+console.log(PlayerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fight();
